fa55684a2f4917c4cdf4979f900587482b3745b2
ibrahim <bra-95@live.fr>
2021-12-27 16:57:29 +0100

    corrected build errors at _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
-import App from 'next/App'
-class MyApp extends App {
-  render() {
+
+function MyApp({ Component, pageProps }){
     const { Component, pageProps } = this.props
 
     const getLayout = Component.getLayout || (page => page)
 
     return getLayout(<Component {...pageProps}></Component>)
-  }
 }
 
 export default MyApp
