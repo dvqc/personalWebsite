e40537e9fec0a59c4d8b4e527715c1ef2759eb15
ibrahim <bra-95@live.fr>
2021-12-30 22:15:19 +0100

    refactored the styles

--- components/Footer.js
+++ components/Footer.js
@@ -7,9 +7,9 @@ export default function Footer() {
                 © 2022 B.Ibrahim.
             </p>
             <div className={styles.links}>
-                <a href="#"><img src="/icons/facebook.svg" /></a>
+                {/* <a href="#"><img src="/icons/facebook.svg" /></a>
                 <a href="#"><img src="/icons/gmail.svg" /></a>
-                <a href="https://github.com/dvqc"><img src="/icons/github.svg" /></a>
+                 */}<a href="https://github.com/dvqc"><img src="/icons/github.svg" /></a>
                 <a href="https://www.linkedin.com/in/ibrahim-benfares-5590a0225/"><img src="/icons/linkedin.svg" /></a>
             </div>
         </footer>
